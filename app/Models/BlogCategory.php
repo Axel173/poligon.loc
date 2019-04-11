e5e97eb0c4ec16e0d94260852289e9d419be0747
axel73 <alex310197@live.com>
2019-04-12 00:24:47 +0400

    мутатуры и аксесуары

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -39,4 +39,25 @@ class BlogCategory extends Model
     {
         return $this->id === BlogCategory::ROOT;
     }
+
+    /**
+     * Пример аксесуара
+     *
+     * @param $valueFromObject
+     * @return bool|false|mixed|string|string[]|null
+     */
+    public function getTitleAttribute($valueFromObject)
+    {
+        return mb_strtoupper($valueFromObject);
+    }
+
+    /**
+     * Пример мутатора
+     *
+     * @param $incomingValue
+     */
+    public function setTitleAttribute($incomingValue)
+    {
+        $this->attributes['title'] = $incomingValue();
+    }
 }
